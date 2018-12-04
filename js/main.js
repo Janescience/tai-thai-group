75f06c3963a0dfa77764dd580a9653f0d8815bff
Jane <janescience.top@gmail.com>
2018-12-04 09:29:15 +0700

    -

--- js/main.js
+++ js/main.js
@@ -381,12 +381,11 @@
 	var selectedClass = "";
 	$(".filter").click(function(){
 		selectedClass = $(this).attr("data-rel");
-		$("#gallery").fadeTo(100, 0.1);
-		$("#gallery").find("div").not("."+selectedClass).find(".box-shadow").fadeOut().removeClass('animation');
+		$(".pics").fadeTo(100, 0.1);
+		$(".pics").not("."+selectedClass).fadeOut().removeClass('animation');
 		setTimeout(function() {
-			$("."+selectedClass).find("div").fadeIn().addClass('box-shadow');
 			$("."+selectedClass).fadeIn().addClass('animation');
-			$("#gallery").fadeTo(300, 1);
+			$(".pics").fadeTo(300, 1);
 		}, 300);
 	});
 
